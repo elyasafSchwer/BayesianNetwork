ac466fc0b04eaad92c5b39854008ca80866534c0
אליסף <@SCHWER_PC>
2019-08-23 13:15:47 +0300

    separate main to functions

--- ex1.java
+++ ex1.java
@@ -9,13 +9,17 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class ex1{
+	
+	public static final String INPUT_FILE_NAME = "input.txt";
+	public static final String OUTPUT_FILE_NAME = "output.txt";
+	
 	public static void main(String[] args)  throws IOException {
 		BayesianNetwork network = null;
 
-		FileReader fileReader = new FileReader("input.txt");
+		FileReader fileReader = new FileReader(INPUT_FILE_NAME);
 		BufferedReader bufferReader = new BufferedReader(fileReader);
 
-		File outputFile = new File("output.txt");
+		File outputFile = new File(OUTPUT_FILE_NAME);
 		outputFile.createNewFile();
 		FileWriter fileWriter = new FileWriter(outputFile);
 		BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
@@ -25,110 +29,149 @@ public class ex1{
 		
 		while(bufferReader.ready()){
 			tokenLine = new StringTokenizer(bufferReader.readLine(), " ,:=");
-			while(tokenLine.countTokens()<1){
+			while(tokenLine.countTokens() < 1){
 				tokenLine = new StringTokenizer(bufferReader.readLine(), " ,:=");
 			}
 			String firstWord = tokenLine.nextToken();
-			if(firstWord.equals("Network")){
-				network = new BayesianNetwork();
-			}
-			if(firstWord.equals("Variables")){
-				while(tokenLine.hasMoreTokens()){
-					network.addVariable(tokenLine.nextToken());
-				}
-			}
-			if(firstWord.equals("Var")){
+			switch (firstWord) {
+			case "Network":
+				network = new BayesianNetwork();				
+				break;
+			case "Variables":
+				addVariable2NetworkFromToken(network, tokenLine);
+				break;
+			case "Var":
 				Variable var = network.getVar(tokenLine.nextToken());
-				tokenLine = new StringTokenizer(bufferReader.readLine(), " ,:=");
-				tokenLine.nextToken();
-				List<String> varValueString = new ArrayList<String>();
-				while(tokenLine.hasMoreTokens()){
-					varValueString.add(tokenLine.nextToken());
-				}
-				network.setValues(var.getName(), varValueString);
-				tokenLine = new StringTokenizer(bufferReader.readLine(), " ,:=");
-				tokenLine.nextToken();
-				String first_parent = tokenLine.nextToken();
-				if(!first_parent.equals("none")){
-					List<Variable> parents = new ArrayList<Variable>();
-					parents.add(network.getVar(first_parent));
-					while(tokenLine.hasMoreTokens()){
-						parents.add(network.getVar(tokenLine.nextToken()));
-					}
-					network.setParents(var.getName(), parents);
-				}
-				bufferReader.readLine();
-				tokenLine = new StringTokenizer(bufferReader.readLine(), " ,:=");
-				while(tokenLine.countTokens()>1){
-					List<VariableCondition> variableState = new ArrayList<VariableCondition>();
-					for(Variable parent : var.getParents()){
-						variableState.add(new VariableCondition(parent, tokenLine.nextToken()));
-					}
-					while(tokenLine.hasMoreTokens()){
-						CptVariableCondition cptVariableState = new CptVariableCondition(var, variableState, tokenLine.nextToken());
-						var.addtoCPT(new CptConditionProbability(cptVariableState, Double.parseDouble(tokenLine.nextToken())));
-					}
-					tokenLine = new StringTokenizer(bufferReader.readLine(), " ,:=");
-				}
-			}
-			if(firstWord.equals("Queries")){
+				setValues2VarFromLine(network, var, bufferReader.readLine());
+				setValues2VarFromBr(network, var, bufferReader);
+				setCptFromBR(var, bufferReader);
+				break;
+			case "Queries":
 				System.out.println(network);
 				String line = bufferReader.readLine();
 				while(line.length() > 1){
-					if(line.substring(0, 2).equals("P(") && line.contains(")")){
-						StringTokenizer query_string = new StringTokenizer(line, "P()|");
-						String query_var_string = query_string.nextToken();
-						VariableCondition query_var = new VariableCondition(network.getVar(query_var_string.split("=")[0]), query_var_string.split("=")[1]);
-						List<VariableCondition> evidence = new ArrayList<VariableCondition>();
-						String evidence_strings = query_string.nextToken();
-						StringTokenizer evidence_strings_token = new StringTokenizer(evidence_strings, ",= ");
-						while(evidence_strings_token.hasMoreTokens()){
-							evidence.add(new VariableCondition(network.getVar(evidence_strings_token.nextToken()), evidence_strings_token.nextToken()));
-						}
-						List<Variable> hiddens = new ArrayList<Variable>();
-						if(query_string.hasMoreElements()){
-							String hidens_string = query_string.nextToken();
-							StringTokenizer hidens_string_token = new StringTokenizer(hidens_string, " ,-");
-							if(hidens_string_token.hasMoreTokens()){
-								while(hidens_string_token.hasMoreElements()){
-									hiddens.add(network.getVar(hidens_string_token.nextToken()));
-								}
-							}
-						}
-						String result = network.P(query_var, evidence, hiddens);
-						System.out.println(result);
-						
-						if(!firstLine) bufferWriter.newLine();
-						bufferWriter.write(result);
-						if(firstLine) firstLine = false;
+					if(isProbabilityQueries(line)){
+						calcProbablility(network, line, bufferWriter, firstLine);
 					}
 					else{
-						StringTokenizer query_string = new StringTokenizer(line, "|");
-						String queries_var_string = query_string.nextToken();
-						StringTokenizer queries_var_string_token = new StringTokenizer(queries_var_string, "- ");
-						Variable var1 = network.getVar(queries_var_string_token.nextToken());
-						Variable var2 = network.getVar(queries_var_string_token.nextToken());
-						List<Variable> givens = new ArrayList<Variable>();
-						if(query_string.hasMoreTokens()){
-							String queries_givens_string = query_string.nextToken();
-							StringTokenizer queries_givens_string_token = new StringTokenizer(queries_givens_string, ", ");
-							while(queries_givens_string_token.hasMoreTokens()){
-								givens.add(network.getVar(queries_givens_string_token.nextToken().split("=")[0]));
-							}
-						}
-						String result = (!network.BayesBall(var1, var2, null, givens, true)) ? "yes" : "no";
-						
-						if(!firstLine) bufferWriter.newLine();
-						bufferWriter.write(result);
-						if(firstLine) firstLine = false;
+						calcIndependent(network, line, bufferWriter, firstLine);
 					}
 					line = (bufferReader.ready()) ? bufferReader.readLine() : "";
 				}
+				break;					
+			default:
+				break;
 			}
 		}
 		bufferReader.close();
 		fileReader.close();
 		bufferWriter.close();
 		fileWriter.close();
+	}
+
+	private static void calcIndependent(BayesianNetwork network, String line, BufferedWriter bufferWriter,
+			boolean firstLine) throws IOException {
+		StringTokenizer query_string = new StringTokenizer(line, "|");
+		String queries_var_string = query_string.nextToken();
+		StringTokenizer queries_var_string_token = new StringTokenizer(queries_var_string, "- ");
+		Variable var1 = network.getVar(queries_var_string_token.nextToken());
+		Variable var2 = network.getVar(queries_var_string_token.nextToken());
+		List<Variable> givens = new ArrayList<Variable>();
+		if(query_string.hasMoreTokens()){
+			String queries_givens_string = query_string.nextToken();
+			StringTokenizer queries_givens_string_token = new StringTokenizer(queries_givens_string, ", ");
+			while(queries_givens_string_token.hasMoreTokens()){
+				givens.add(network.getVar(queries_givens_string_token.nextToken().split("=")[0]));
+			}
+		}
+		String result = (!network.BayesBall(var1, var2, null, givens, true)) ? "yes" : "no";
+		
+		if(!firstLine) bufferWriter.newLine();
+		bufferWriter.write(result);
+		if(firstLine) firstLine = false;
+	}
+
+	private static void calcProbablility(BayesianNetwork network, String line, BufferedWriter bufferWriter, 
+			boolean firstLine) throws IOException {
+		StringTokenizer query_string = new StringTokenizer(line, "P()|");
+		String query_var_string = query_string.nextToken();
+		VariableCondition query_var = new VariableCondition(network.getVar(query_var_string.split("=")[0]), query_var_string.split("=")[1]);
+		List<VariableCondition> evidence = new ArrayList<VariableCondition>();
+		String evidence_strings = query_string.nextToken();
+		StringTokenizer evidence_strings_token = new StringTokenizer(evidence_strings, ",= ");
+		while(evidence_strings_token.hasMoreTokens()){
+			evidence.add(new VariableCondition(network.getVar(evidence_strings_token.nextToken()), evidence_strings_token.nextToken()));
+		}
+		List<Variable> hiddens = new ArrayList<Variable>();
+		if(query_string.hasMoreElements()){
+			String hidens_string = query_string.nextToken();
+			StringTokenizer hidens_string_token = new StringTokenizer(hidens_string, " ,-");
+			if(hidens_string_token.hasMoreTokens()){
+				while(hidens_string_token.hasMoreElements()){
+					hiddens.add(network.getVar(hidens_string_token.nextToken()));
+				}
+			}
+		}
+		String result = network.P(query_var, evidence, hiddens);
+		System.out.println(result);
+		
+		if(!firstLine) bufferWriter.newLine();
+		bufferWriter.write(result);
+		if(firstLine) firstLine = false;
+	}
+
+	private static boolean isProbabilityQueries(String line) {
+		return line.substring(0, 2).equals("P(") && line.contains(")");
+	}
+
+	private static void setValues2VarFromBr(BayesianNetwork network, Variable var, BufferedReader bufferReader)
+			throws IOException {
+		StringTokenizer tokenLine = new StringTokenizer(bufferReader.readLine(), " ,:=");
+		tokenLine.nextToken();
+		String first_parent = tokenLine.nextToken();
+		if(!first_parent.equals("none")){
+			setParents2VarFromLine(network, var, first_parent, tokenLine);
+		}		
+	}
+
+	private static void setCptFromBR(Variable var, BufferedReader bufferReader) throws IOException {
+		bufferReader.readLine();
+		StringTokenizer tokenLine = new StringTokenizer(bufferReader.readLine(), " ,:=");
+		while(tokenLine.countTokens() > 1){
+			List<VariableCondition> variableState = new ArrayList<VariableCondition>();
+			for(Variable parent : var.getParents()){
+				variableState.add(new VariableCondition(parent, tokenLine.nextToken()));
+			}
+			while(tokenLine.hasMoreTokens()){
+				CptVariableCondition cptVariableState = new CptVariableCondition(var, variableState, tokenLine.nextToken());
+				var.addtoCPT(new CptConditionProbability(cptVariableState, Double.parseDouble(tokenLine.nextToken())));
+			}
+			tokenLine = new StringTokenizer(bufferReader.readLine(), " ,:=");
+		}
+	}
+
+	private static void setParents2VarFromLine(BayesianNetwork network, Variable var, String first_parent, StringTokenizer tokenLine) {
+		List<Variable> parents = new ArrayList<Variable>();
+		parents.add(network.getVar(first_parent));
+		while(tokenLine.hasMoreTokens()){
+			parents.add(network.getVar(tokenLine.nextToken()));
+		}
+		network.setParents(var.getName(), parents);
+	}
+
+	private static void setValues2VarFromLine(BayesianNetwork network, Variable var, String line) {
+		StringTokenizer tokenLine = new StringTokenizer(line, " ,:=");
+		tokenLine.nextToken();
+		List<String> varValueString = new ArrayList<String>();
+		while(tokenLine.hasMoreTokens()){
+			varValueString.add(tokenLine.nextToken());
+		}
+		network.setValues(var.getName(), varValueString);
+	}
+
+	private static void addVariable2NetworkFromToken(BayesianNetwork network, StringTokenizer tokenLine) {
+		while(tokenLine.hasMoreTokens()){
+			network.addVariable(tokenLine.nextToken());
+		}
 	}
 }
